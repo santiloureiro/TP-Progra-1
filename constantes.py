b42a22fd2943dd6a1a52a8c6eb3e50cdd16a713b
sloureiro1 <sloureiro1@lenovo.com>
2026-06-12 21:15:11 -0300

    Añadir archivos de constantes

---
+++ constantes.py
@@ -0,0 +1,7 @@
+ARCHIVO_PEDIDOS = "./carpeta_archivos/pedidos.json"
+ARCHIVO_TECNICOS = "./carpeta_archivos/tecnicos.json"
+PRIORIDADES = ("Baja", "Media", "Alta", "Urgente")
+TIPOSDETRABAJOS = ("Aire acondicionado", "Electricidad", "Soporte tecnico")
+REGEXTELEFONO = r"([0-9]{2,4})(15)([0-9]{6,8})"
+REGEXPEDIDOS = r"([0-9]*)"
+REGEXTECNICOS = r"([A-Za-z]*)"
